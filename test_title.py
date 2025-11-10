a86d6e4af6734beeb55964e088aee462b856b1b2
Phantom-Praise <praiseben13@gmail.com>
2025-11-10 13:55:23 -0500

    Add grabdocs test title

---
+++ test_title.py
@@ -0,0 +1,21 @@
+# pytest -s
+
+import re 
+from playwright.sync_api import sync_playwright, Page, expect
+
+def test_has_title(): 
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+
+        page.goto("https://grabdocs.com")
+
+        print(f"The Page Title Is {page.title()}")
+        expect(page).to_have_title(re.compile("GrabDocs"))
+
+        page.screenshot(path="img/test_has_title.png")
+        print("Screenshot taken and saved as test_has_title.png")
+
+        browser.close()
+
+test_has_title()
